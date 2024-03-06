3a25bdf6fc3dbe1a66278173d1468194cb72312b
salahvk <salahkoyilandy@gmail.com>
2024-03-06 22:50:56 +0530

    asset modified

--- lib/utils/constants/assets.dart
+++ lib/utils/constants/assets.dart
@@ -3,62 +3,62 @@ class Assets {
   
   /// Assets for assetsAudioCollision
   /// assets/audio/collision.wav
-  static const String assetsAudioCollision = "assets/audio/collision.wav";
+  static const String assetsAudioCollision = "collision.wav";
 
   /// Assets for assetsAudioFly
   /// assets/audio/fly.wav
-  static const String assetsAudioFly = "assets/audio/fly.wav";
+  static const String assetsAudioFly = "fly.wav";
 
   /// Assets for assetsAudioPoint
   /// assets/audio/point.wav
-  static const String assetsAudioPoint = "assets/audio/point.wav";
+  static const String assetsAudioPoint = "point.wav";
 
   /// Assets for assetsFontsGame
   /// assets/fonts/Game.ttf
-  static const String assetsFontsGame = "assets/fonts/Game.ttf";
+  static const String assetsFontsGame = "Game.ttf";
 
   /// Assets for assetsImagesBackground
   /// assets/images/background.png
-  static const String assetsImagesBackground = "assets/images/background.png";
+  static const String assetsImagesBackground = "background.png";
 
   /// Assets for assetsImagesBirdDownflap
   /// assets/images/bird_downflap.png
-  static const String assetsImagesBirdDownflap = "assets/images/bird_downflap.png";
+  static const String assetsImagesBirdDownflap = "bird_downflap.png";
 
   /// Assets for assetsImagesBirdMidflap
   /// assets/images/bird_midflap.png
-  static const String assetsImagesBirdMidflap = "assets/images/bird_midflap.png";
+  static const String assetsImagesBirdMidflap = "bird_midflap.png";
 
   /// Assets for assetsImagesBirdUpflap
   /// assets/images/bird_upflap.png
-  static const String assetsImagesBirdUpflap = "assets/images/bird_upflap.png";
+  static const String assetsImagesBirdUpflap = "bird_upflap.png";
 
   /// Assets for assetsImagesClouds
   /// assets/images/clouds.png
-  static const String assetsImagesClouds = "assets/images/clouds.png";
+  static const String assetsImagesClouds = "clouds.png";
 
   /// Assets for assetsImagesGameover
   /// assets/images/gameover.png
-  static const String assetsImagesGameover = "assets/images/gameover.png";
+  static const String assetsImagesGameover = "gameover.png";
 
   /// Assets for assetsImagesGround
   /// assets/images/ground.png
-  static const String assetsImagesGround = "assets/images/ground.png";
+  static const String assetsImagesGround = "ground.png";
 
   /// Assets for assetsImagesMenu
   /// assets/images/menu.jpg
-  static const String assetsImagesMenu = "assets/images/menu.jpg";
+  static const String assetsImagesMenu = "menu.jpg";
 
   /// Assets for assetsImagesMessage
   /// assets/images/message.png
-  static const String assetsImagesMessage = "assets/images/message.png";
+  static const String assetsImagesMessage = "message.png";
 
   /// Assets for assetsImagesPipe
   /// assets/images/pipe.png
-  static const String assetsImagesPipe = "assets/images/pipe.png";
+  static const String assetsImagesPipe = "pipe.png";
 
   /// Assets for assetsImagesPipeRotated
   /// assets/images/pipe_rotated.png
-  static const String assetsImagesPipeRotated = "assets/images/pipe_rotated.png";
+  static const String assetsImagesPipeRotated = "pipe_rotated.png";
 }
 
